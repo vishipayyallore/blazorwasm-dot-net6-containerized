0342b1c2c05ba1bc8a357a1d25bf4d2ec8551af5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-11 08:11:03 +0530

    Books.API

--- source/webapi/Books.API/Program.cs
+++ source/webapi/Books.API/Program.cs
@@ -1,6 +1,22 @@
+using BooksStore.Bll;
+using BooksStore.CacheDal;
+using BooksStore.CacheDal.Persistence;
+using BooksStore.Core.Configuration;
+using BooksStore.Core.Interfaces;
+using BooksStore.SqlDal;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<DataStoreSettings>(builder.Configuration.GetSection(nameof(DataStoreSettings)));
+builder.Services.AddSingleton<IDataStoreSettings>(sp => sp.GetRequiredService<IOptions<DataStoreSettings>>().Value);
+
+builder.Services.AddSingleton<IRedisCacheDbContext, RedisCacheDbContext>();
+builder.Services.AddScoped<IBooksBll, BooksBll>();
+builder.Services.AddScoped<IBookCacheRepository, BookCacheRepository>();
+
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
